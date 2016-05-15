bc5046551c8453d63e2ce4f07cb2e3d44279341a
Ralph Maamari <ralphpal@hotmail.com>
2016-05-14 23:54:22 -0400

    Updated the entertainment_center.py file
    Added Comments and made the video link dynamic

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,14 +10,17 @@ movie = tmdb.Movies(100)
 response = movie.info()
 
 while count < 5:
+    # Grabs a random movie from the database
     movie = tmdb.Movies(random.randint(2, 150))
     response = movie.info()
+    # If there is no video file attached Rick Roll the user
     if (movie.video is False):
         movie.video = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    # Add the random movie to our Array of Movies
     movies.append(
         media.Movie(movie.title, movie.overview,
                     "https://image.tmdb.org/t/p/original/" +
                     movie.poster_path, movie.video))
     count += 1
-
+# Open and generate the HTML file of our movies
 fresh_tomatoes.open_movies_page(movies)
